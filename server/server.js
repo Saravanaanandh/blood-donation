865c6c3904e25539ed41b2e5720de2e2dd14e6b4
Saravanaanandh <saravanaanandh2006@gmail.com>
2025-03-02 20:06:46 +0530

    home page linked

--- server/server.js
+++ server/server.js
@@ -23,10 +23,26 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true })); 
 app.use(cookieParser())
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,'./../client/dist')))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+    })
+}
+
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/request',verifyJWT,bloodReqRouter)
 app.use('/api/v1/donate',verifyJWT,donorRouter)
 
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,'./../client/dist')))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+    })
+}
 app.listen(PORT, ()=>{
     console.log(`server running on PORT ${PORT}`)
     connectDB()
