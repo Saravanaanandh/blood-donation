cd2af6daeaf9083c765220a93a84128a5d387eb5
Saravanaanandh <saravanaanandh2006@gmail.com>
2025-08-25 22:08:28 +0530

    cross-site updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,7 +12,7 @@ export const signupController = async (req, res)=>{
         const token = user.createJWT() 
         user.token = token 
 
-        res.cookie('jwt',token,{httpOnly:true,maxAge:30*24*60*60*1000, secure:true, sameSite:"None"})
+        res.cookie('jwt',token,{httpOnly:true,maxAge:30*24*60*60*1000, secure:true, sameSite:"none"})
         res.status(201).json(user) 
     }catch(err){
         res.status(400).json({message:err.name})
@@ -32,7 +32,7 @@ export const loginController = async (req, res)=>{
     const token = user.createJWT()
     user.token = token
 
-    res.cookie('jwt',token,{httpOnly:true,maxAge:30*24*60*60*1000, secure:true, sameSite:"None"})
+    res.cookie('jwt',token,{httpOnly:true,maxAge:30*24*60*60*1000, secure:true, sameSite:"none"})
     res.status(200).json(user)
 }
 
@@ -69,7 +69,7 @@ export const logoutController = async (req, res)=>{
     if(!user) return res.status(404).json({message:"user not found"}) 
     user.token = ""
 
-    res.clearCookie('jwt',{httpOnly:true, secure:true, sameSite:"None"})
+    res.clearCookie('jwt',{httpOnly:true, secure:true, sameSite:"none"})
     res.status(204).json({message:"user logout successfully"})
 } 
 
