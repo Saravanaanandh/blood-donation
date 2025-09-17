c7a352c7835f59b9bf1086669b30c78e8417b59f
saravanawebdev <142965017+Saravanaanandh@users.noreply.github.com>
2025-09-17 09:38:25 +0530

    Update axios.jsx

--- client/src/lib/axios.jsx
+++ client/src/lib/axios.jsx
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 export const socket = io(
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : "https://your-backend.onrender.com",
+    ? "http://localhost:5000/api/v1"
+    :"/api/v1",
   {
     transports: ["websocket"], // 👈 belongs here, not in axios
     withCredentials: true,
