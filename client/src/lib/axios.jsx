07119dac28dd7cf09ba893513896a9b0eae65613
saravanawebdev <142965017+Saravanaanandh@users.noreply.github.com>
2025-09-17 09:53:17 +0530

    Update axios.jsx

--- client/src/lib/axios.jsx
+++ client/src/lib/axios.jsx
@@ -1,11 +1,5 @@
-import { io } from "socket.io-client";
-
-export const socket = io(
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/v1"
-    :"/api/v1",
-  {
-    transports: ["websocket"], // 👈 belongs here, not in axios
-    withCredentials: true,
-  }
-);
+import axios from "axios"
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE==="development"?'http://localhost:5000/api/v1':'/api/v1', 
+    withCredentials:true
+})
